e1b3d9d14ee034ef459e5fa4a6a78708e22239ac
Leonardo <leonardolafa17@gmail.com>
2025-08-21 13:17:13 -0300

    modificando o codigo

--- pokedex/assets/js/poke-API.js
+++ pokedex/assets/js/poke-API.js
@@ -1,12 +1,24 @@
 
 const pokeAPI={}
 
-pokeAPI.getPokemonsDetails = (pokemon) => {
-    return fetch(pokemon.url).then((response) => response.json())
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon();
+    pokemon.number = pokeDetail.order;
+    pokemon.name = pokeDetail.name;
+    pokemon.types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    pokemon.type = pokemon.types[0];
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
 
+    return pokemon;
+}    
+
+pokeAPI.getPokemonsDetails = (pokemon) => {
+    return fetch(pokemon.url)
+            .then((response) => response.json())
+            .then(convertPokeApiDetailToPokemon)
 }
 
-pokeAPI.getPokemonData = (offset=0,limit=10)=>{
+pokeAPI.getPokemonData = (offset=0,limit=6)=>{
    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
         .then(response => response.json())
